2fec2339b2cbde781b9e4cb1646447cbe27dd4e9
Vitalii Roditieliev <Vitalii.Roditieliev@wirexapp.com>
2019-01-03 16:57:16 +0200

    proj structure update, headers visibility fixed

--- WirexUtils/WirexUtils.h
+++ WirexUtils/WirexUtils.h
@@ -17,4 +17,4 @@ FOUNDATION_EXPORT const unsigned char WirexUtilsVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WirexUtils/PublicHeader.h>
 
 #import "CryptoFacade.h"
-
+#import "CardIO.h"
